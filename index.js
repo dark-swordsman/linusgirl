96eb077f9a4ba213dd5d3e602769a4336b561043
Kyle Barr <kylebarr54321@gmail.com>
2017-10-27 21:18:48 -0400

    port

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+var config = require('./config')(process.env.NODE_ENV);
+var appPort = process.env.PORT || config.port;
+
 app.use('/', express.static('./src'));
 
-app.listen(8080, function(){
-  console.log('Linus is being a valley girl on 8080');
+app.listen(appPort, function(){
+  console.log('Linus is being a valley girl on ' + appPort);
 });
